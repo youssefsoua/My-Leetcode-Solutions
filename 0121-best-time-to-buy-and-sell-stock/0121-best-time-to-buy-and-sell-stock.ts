7bdcb228174196993d4c7a0d3baf9dfca9216349
Youssef Soua <youssefsoua94@gmail.com>
2023-08-20 13:56:30 +0100

    Time: 64 ms (97.48%), Space: 52.1 MB (34.58%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -1,11 +1,8 @@
 function maxProfit(prices: number[]): number {
-    const n = prices.length
-    if(n === 1) return 0
-
     let max = 0
     let min = prices[0]
 
-    for(let i = 1; i<n; i++){
+    for(let i = 1; i<prices.length; i++){
         max = Math.max(max, prices[i] - min)
         min = Math.min(min, prices[i])
     }
