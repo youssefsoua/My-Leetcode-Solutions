9715a7a51cfe8309bef4e4d3a0ca2b1bb286b0d0
Youssef Soua <youssefsoua94@gmail.com>
2023-08-23 01:04:10 +0100

    Time: 66 ms (85.11%) | Memory: 44.7 MB (97.58%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function hasCycle(head: ListNode | null): boolean {
+    let fast = head
+    let slow = head
+
+    while(fast?.next){
+        fast = fast.next.next
+        slow = slow.next
+        if(fast === slow) return true
+    }
+
+    return false
+};
