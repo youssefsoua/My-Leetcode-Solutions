420f1f9493d00e94fb362b688808ab133039ed8f
Youssef Soua <youssefsoua94@gmail.com>
2023-08-27 21:25:18 +0100

    Time: 74 ms (43.07%) | Memory: 47.3 MB (36.76%) - LeetSync

---
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.ts
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function diameterOfBinaryTree(root: TreeNode | null): number {
+    let maxDiameter = 0;
+
+    const getDepth = (node: TreeNode | null): number => {
+        if (!node) return 0;
+        
+        const leftDepth = getDepth(node.left);
+        const rightDepth = getDepth(node.right);
+
+        maxDiameter = Math.max(maxDiameter, leftDepth + rightDepth);
+
+        return Math.max(leftDepth, rightDepth) + 1;
+    };
+
+    getDepth(root);
+
+    return maxDiameter;
+}
