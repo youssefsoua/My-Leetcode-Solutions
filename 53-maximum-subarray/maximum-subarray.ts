7527e74ac3f34f169086f7a8ce168d7b32f70c07
Youssef Soua <youssefsoua94@gmail.com>
2023-08-28 12:51:51 +0100

    Time: 81 ms (54.05%) | Memory: 51.6 MB (94.88%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.ts
+++ 53-maximum-subarray/maximum-subarray.ts
@@ -19,3 +19,24 @@ function maxSubArray(nums: number[]): number {
 
     return max;
 }
+
+/*
+function maxSubArray(nums: number[]): number {
+    let maxEndingHere = nums[0];
+    let maxSoFar = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
+        maxSoFar = Math.max(maxSoFar, maxEndingHere);
+    }
+
+    return maxSoFar;
+}
+
+
+Kadane's algorithm works by keeping track of the maximum subarray ending at each position and the maximum subarray found so far. It iterates through the array once, calculating the maximum ending at the current position and updating the overall maximum if needed.
+
+Both the original sliding window solution and the Kadane's algorithm approach are valid and efficient. The choice between them often comes down to personal preference and readability. Kadane's algorithm is known for its simplicity and is often preferred for its elegant solution.
+
+
+*/
