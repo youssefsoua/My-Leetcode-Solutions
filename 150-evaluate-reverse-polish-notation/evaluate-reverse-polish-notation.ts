f04b855a83f7c31153faa5f631951d13d3aae428
Youssef Soua <youssefsoua94@gmail.com>
2023-09-01 18:26:10 +0100

    Time: 64 ms (72.49%) | Memory: 45.6 MB (57.74%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.ts
@@ -0,0 +1,31 @@
+function evalRPN(tokens: string[]): number {
+    const stack: number[] = [];
+
+    for (const token of tokens) {
+        if (isNaN(parseInt(token))) {
+            const num2 = stack.pop()!;
+            const num1 = stack.pop()!;
+            stack.push(applyOperator(token, num1, num2));
+        } else {
+            stack.push(parseInt(token));
+        }
+    }
+
+    return stack[0];
+}
+
+
+function applyOperator(operator: string, num1: number, num2: number): number {
+    switch (operator) {
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+        case "*":
+            return num1 * num2;
+        case "/":
+            return Math.trunc(num1 / num2);
+        default:
+            throw new Error("Invalid operator: " + operator);
+    }
+}
