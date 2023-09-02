4690eac3bfb4e300a5bdae0ff2a33df406dcc17b
Youssef Soua <youssefsoua94@gmail.com>
2023-09-02 17:24:21 +0100

    Time: 89 ms (49.65%) | Memory: 51.1 MB (43.44%) - LeetSync

--- 200-number-of-islands/number-of-islands.ts
+++ 200-number-of-islands/number-of-islands.ts
@@ -1,27 +1,27 @@
 function numIslands(grid: string[][]): number {
-    const m = grid.length
-    const n = grid[0].length
-    let count = 0
-    const directions = [[1,0],[-1,0],[0,1],[0,-1]]
+    const m = grid.length;
+    const n = grid[0].length;
+    let count = 0;
+    const directions = [[1,0],[-1,0],[0,1],[0,-1]];
 
-    for(let i = 0; i<m; i++){
-        for(let j = 0; j<n; j++){
+    for(let i = 0; i < m; i++){
+        for(let j = 0; j < n; j++){
             if(grid[i][j] === "1"){
-                const stack = [[i,j]]
+                const stack = [[i,j]];
                 while(stack.length){
-                    const [x,y] = stack.pop()
-                    grid[x][y] = "0"
+                    const [x,y] = stack.pop();
+                    grid[x][y] = "0";
                     for(const direction of directions){
-                        const [v,w] = direction
-                        const newX = v+x
-                        const newY = w+y
-                        if(newX>=0 && newX<m && newY>=0 && newY<n && grid[newX][newY] === "1") stack.push([newX,newY])
+                        const [v,w] = direction;
+                        const newX = v+x;
+                        const newY = w+y;
+                        if(newX >= 0 && newX < m && newY >= 0 && newY < n && grid[newX][newY] === "1") stack.push([newX,newY]);
                     }
                 }
-                count++
+                count++;
             }
         }
     }
 
-    return count
+    return count;
 };
