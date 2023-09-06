c533c395026664713a1b5899aa8e75e00c6b37f7
Youssef Soua <youssefsoua94@gmail.com>
2023-09-06 10:43:03 +0100

    Time: 54 ms (79.66%) | Memory: 44.8 MB (10.80%) - LeetSync

--- 75-sort-colors/sort-colors.ts
+++ 75-sort-colors/sort-colors.ts
@@ -1,30 +1,49 @@
 /**
  Do not return anything, modify nums in-place instead.
  */
+
 function sortColors(nums: number[]): void {
-    const n = nums.length
-    let zero = 0
-    let two = n-1
-    let index = n - 1
+    const n: number = nums.length;
+    let zero: number = 0;
+    let two: number = n - 1;
+    let index: number = n - 1;
 
-    while(index>zero){
-        while(index>zero && nums[zero] === 0) zero++
-        if(nums[index] === 0){
-            const temp = nums[zero]
-            nums[zero] = nums[index]
-            nums[index] = temp
-        }           
-        index--
+    while (index > zero) {
+        while (index > zero && nums[zero] === 0) zero++;
+        if (nums[index] === 0)
+            [nums[index], nums[zero]] = [nums[zero], nums[index]];
+        index--;
     }
 
-    while(index<two){
-        while(index<two && nums[two] === 2) two--
-        if(nums[index] === 2){
-            const temp = nums[two]
-            nums[two] = nums[index]
-            nums[index] = temp
-        }           
-        index++
+    while (index < two) {
+        while (index < two && nums[two] === 2) two--;
+        if (nums[index] === 2)
+            [nums[index], nums[two]] = [nums[two], nums[index]];
+        index++;
     }
+}
 
-};
+
+/**
+function sortColors(nums: number[]): void {
+    const n: number = nums.length;
+    let zero: number = 0;
+    let two: number = n - 1;
+    let index: number = 0;
+
+    while (index <= two) {
+        if (nums[index] === 0) {
+            // Swap nums[index] with nums[zero]
+            [nums[index], nums[zero]] = [nums[zero], nums[index]];
+            zero++;
+            index++;
+        } else if (nums[index] === 2) {
+            // Swap nums[index] with nums[two]
+            [nums[index], nums[two]] = [nums[two], nums[index]];
+            two--;
+        } else {
+            index++;
+        }
+    }
+}
+ */
