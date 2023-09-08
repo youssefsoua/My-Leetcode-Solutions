8f408db6af81b7de35fde87ec8082cd995bb29b4
Youssef Soua <youssefsoua94@gmail.com>
2023-09-08 17:30:45 +0100

    Time: 77 ms (25.55%) | Memory: 46.2 MB (39.26%) - LeetSync

--- 46-permutations/permutations.ts
+++ 46-permutations/permutations.ts
@@ -1,17 +1,19 @@
 function permute(nums: number[]): number[][] {
+    const results: number[][] = [];
 
-    const results: number[][] = []
-
-    const findPermutations = (nums: number[], permutations: number[]) =>{
-        if(nums.length === 0) results.push([...permutations])
-        for(const num of nums){
-            permutations.push(num)
-            findPermutations(nums.filter((e)=>e !== num), permutations)
-            permutations.pop()
+    const findPermutations = (nums: number[], permutations: number[]) => {
+        if (nums.length === 0) results.push([...permutations]);
+        for (const num of nums) {
+            permutations.push(num);
+            findPermutations(
+                nums.filter((e) => e !== num),
+                permutations,
+            );
+            permutations.pop();
         }
-    }
+    };
 
-    findPermutations(nums, [])
+    findPermutations(nums, []);
 
-    return results
-};
+    return results;
+}
