e5863ecccd2266959d5a337ec8323b0ce48bdd3e
Youssef Soua <youssefsoua94@gmail.com>
2023-09-10 20:15:31 +0100

    Time: 64 ms (41.98%) | Memory: 44.3 MB (66.68%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.ts
+++ 14-longest-common-prefix/longest-common-prefix.ts
@@ -1,22 +1,43 @@
 function longestCommonPrefix(strs: string[]): string {
-    const dictionary = {};
+    if (strs.length === 0) return "";
+
     let result = "";
+    
+    for (let i = 0; i < strs[0].length; i++) {
+        const char = strs[0][i];
 
-    for (const str of strs) {
-        let current = dictionary;
-        for (const char of str) {
-            if (!current[char]) current[char] = { count: 0 };
-            current[char].count++;
-            current = current[char];
+        for (let j = 1; j < strs.length; j++) {
+            if (i >= strs[j].length || strs[j][i] !== char) {
+                return result;
+            }
         }
-    }
 
-    let current = dictionary;
-    for (const char of strs[0]) {
-        if (current[char].count !== strs.length) break;
         result += char;
-        current = current[char];
     }
 
     return result;
 }
+
+
+// function longestCommonPrefix(strs: string[]): string {
+//     const dictionary = {};
+//     let result = "";
+
+//     for (const str of strs) {
+//         let current = dictionary;
+//         for (const char of str) {
+//             if (!current[char]) current[char] = { count: 0 };
+//             current[char].count++;
+//             current = current[char];
+//         }
+//     }
+
+//     let current = dictionary;
+//     for (const char of strs[0]) {
+//         if (current[char].count !== strs.length) break;
+//         result += char;
+//         current = current[char];
+//     }
+
+//     return result;
+// }
