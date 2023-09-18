dfb2e6c3ec487cee7fa4b1a259ef85540505a6b4
Youssef Soua <youssefsoua94@gmail.com>
2023-09-18 17:03:57 +0100

    Time: 59 ms (63.95%) | Memory: 44.7 MB (47.84%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.ts
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.ts
@@ -12,28 +12,51 @@
  * }
  */
 
+
+
 function rightSideView(root: TreeNode | null): number[] {
     const result: number[] = [];
 
-    const levels: number[][] = [];
+    const dfs = (node: TreeNode | null, level: number): void => {
+        if (!node) return;
 
-    const getLevels = (root: TreeNode | null, index: number = 0): void => {
-        if (root) {
-            if (levels[index]) {
-                levels[index].push(root.val);
-            } else {
-                levels[index] = [root.val];
-            }
-            getLevels(root.right, index + 1);
-            getLevels(root.left, index + 1);
+        if (result.length === level) {
+            result.push(node.val);
         }
+
+        dfs(node.right, level + 1);
+        dfs(node.left, level + 1);
     };
 
-    getLevels(root);
-
-    for (const level of levels) {
-        result.push(level[0]);
-    }
+    dfs(root, 0);
 
     return result;
 }
+
+
+
+// function rightSideView(root: TreeNode | null): number[] {
+//     const result: number[] = [];
+
+//     const levels: number[][] = [];
+
+//     const getLevels = (root: TreeNode | null, index: number = 0): void => {
+//         if (root) {
+//             if (levels[index]) {
+//                 levels[index].push(root.val);
+//             } else {
+//                 levels[index] = [root.val];
+//             }
+//             getLevels(root.right, index + 1);
+//             getLevels(root.left, index + 1);
+//         }
+//     };
+
+//     getLevels(root);
+
+//     for (const level of levels) {
+//         result.push(level[0]);
+//     }
+
+//     return result;
+// }
