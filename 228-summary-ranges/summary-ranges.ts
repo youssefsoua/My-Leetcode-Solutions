1a79f72634aa7462fb15ce482d6ceaf6fe8a365d
Youssef Soua <youssefsoua94@gmail.com>
2023-09-27 20:10:26 +0100

    Time: 53 ms (65.33%) | Memory: 42.8 MB (61.60%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.ts
@@ -0,0 +1,19 @@
+function summaryRanges(nums: number[]): string[] {
+    const result: string[] = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        let start = nums[i];
+
+        while (i < nums.length - 1 && nums[i] + 1 === nums[i + 1]) {
+            i++;
+        }
+
+        if (start === nums[i]) {
+            result.push(start.toString());
+        } else {
+            result.push(`${start}->${nums[i]}`);
+        }
+    }
+
+    return result;
+}
