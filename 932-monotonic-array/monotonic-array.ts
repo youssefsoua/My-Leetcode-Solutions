1df24216876cf40f5abb3fdb5c656f5d33e7f73f
Youssef Soua <youssefsoua94@gmail.com>
2023-09-29 09:35:06 +0100

    Time: 79 ms (61.29%) | Memory: 54.2 MB (95.16%) - LeetSync

--- 932-monotonic-array/monotonic-array.ts
+++ 932-monotonic-array/monotonic-array.ts
@@ -21,3 +21,25 @@ function isMonotonic(nums: number[]): boolean {
 
     return true;
 }
+
+
+/*
+function isMonotonic(nums: number[]): boolean {
+    let increasing = true;
+    let decreasing = true;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > nums[i - 1]) {
+            decreasing = false;
+        } else if (nums[i] < nums[i - 1]) {
+            increasing = false;
+        }
+
+        if (!increasing && !decreasing) {
+            return false;
+        }
+    }
+
+    return true;
+}
+ */
