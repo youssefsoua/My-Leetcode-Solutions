f62d741b62c500b7046e5b3ec41de926367a631c
Youssef Soua <youssefsoua94@gmail.com>
2023-09-30 13:31:55 +0100

    Time: 50 ms (91.25%) | Memory: 44.5 MB (72.62%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.ts
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.ts
@@ -1,28 +1,47 @@
 function reverseWords(s: string): string {
-    const result: string[] = [];
-    let right = s.length - 1;
-    let left = right;
+    const words: string[] = s.split(" ");
+    let result: string = "";
 
-    while (right >= 0) {
-        while (right >= 0 && s[right] === ' ') {
-            right--; // Skip trailing spaces
-            left = right; // Reset the left pointer
-        }
-
-        while (left >= 0 && s[left] !== ' ') {
-            left--; // Move the left pointer to find the beginning of a word
-        }
-
-        const word = s.slice(left + 1, right + 1);
+    for (let i = words.length - 1; i >= 0; i--) {
+        const word = words[i].trim();
         if (word) {
-            result.push(word);
+            if (result) {
+                result += " " + word;
+            } else {
+                result = word;
+            }
         }
-
-        right = left; // Move the right pointer to the end of the next word
     }
 
-    return result.join(' ');
+    return result;
 }
+
+
+// function reverseWords(s: string): string {
+//     const result: string[] = [];
+//     let right = s.length - 1;
+//     let left = right;
+
+//     while (right >= 0) {
+//         while (right >= 0 && s[right] === ' ') {
+//             right--; // Skip trailing spaces
+//             left = right; // Reset the left pointer
+//         }
+
+//         while (left >= 0 && s[left] !== ' ') {
+//             left--; // Move the left pointer to find the beginning of a word
+//         }
+
+//         const word = s.slice(left + 1, right + 1);
+//         if (word) {
+//             result.push(word);
+//         }
+
+//         right = left; // Move the right pointer to the end of the next word
+//     }
+
+//     return result.join(' ');
+// }
 
 
 
