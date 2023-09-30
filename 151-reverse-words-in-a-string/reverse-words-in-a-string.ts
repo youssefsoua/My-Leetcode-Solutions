38ae723266f0c722fe1646d255f8b2a3cb5d251c
Youssef Soua <youssefsoua94@gmail.com>
2023-09-30 13:18:56 +0100

    Time: 64 ms (27.67%) | Memory: 44.5 MB (72.62%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.ts
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.ts
@@ -1,10 +1,38 @@
 function reverseWords(s: string): string {
-    return s
-        .split(" ")
-        .filter(word => word.trim() !== "")
-        .reverse()
-        .join(" ");
+    const result: string[] = [];
+    let right = s.length - 1;
+    let left = right;
+
+    while (right >= 0) {
+        while (right >= 0 && s[right] === ' ') {
+            right--; // Skip trailing spaces
+            left = right; // Reset the left pointer
+        }
+
+        while (left >= 0 && s[left] !== ' ') {
+            left--; // Move the left pointer to find the beginning of a word
+        }
+
+        const word = s.slice(left + 1, right + 1);
+        if (word) {
+            result.push(word);
+        }
+
+        right = left; // Move the right pointer to the end of the next word
+    }
+
+    return result.join(' ');
 }
+
+
+
+// function reverseWords(s: string): string {
+//     return s
+//         .split(" ")
+//         .filter(word => word.trim() !== "")
+//         .reverse()
+//         .join(" ");
+// }
 
 
 /*
