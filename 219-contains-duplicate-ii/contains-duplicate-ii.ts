b2a2c488d5cdd7985aea0d52f8533fedf0d66972
Youssef Soua <youssefsoua94@gmail.com>
2023-10-01 23:04:49 +0100

    Time: 85 ms (74.13%) | Memory: 53.5 MB (83.49%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.ts
+++ 219-contains-duplicate-ii/contains-duplicate-ii.ts
@@ -1,15 +1,34 @@
 function containsNearbyDuplicate(nums: number[], k: number): boolean {
-  const hashMap = new Map<number, number>();
+  const numSet = new Set<number>();
 
   for (let i = 0; i < nums.length; i++) {
-    const exist = hashMap.get(nums[i]);
-
-    if (exist !== undefined) {
-      if (i - k <= exist) return true;
+    if (i > k) {
+      numSet.delete(nums[i - k - 1]); // Remove the number outside the window
     }
 
-    hashMap.set(nums[i], i);
+    if (numSet.has(nums[i])) {
+      return true; // Found a duplicate within the window
+    }
+
+    numSet.add(nums[i]); // Add the current number to the set
   }
 
   return false;
 }
+
+
+// function containsNearbyDuplicate(nums: number[], k: number): boolean {
+//   const hashMap = new Map<number, number>();
+
+//   for (let i = 0; i < nums.length; i++) {
+//     const exist = hashMap.get(nums[i]);
+
+//     if (exist !== undefined) {
+//       if (i - k <= exist) return true;
+//     }
+
+//     hashMap.set(nums[i], i);
+//   }
+
+//   return false;
+// }
