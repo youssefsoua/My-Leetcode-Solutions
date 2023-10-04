7fc1355a2e6c29d1f4ffd8c342f49573d33ddcfc
Youssef Soua <youssefsoua94@gmail.com>
2023-10-04 22:48:29 +0100

    Time: 50 ms (82.71%) | Memory: 43.4 MB (86.84%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function postorderTraversal(root: TreeNode | null, result: number[] = []): number[] {
+    if(!root) return result
+
+    postorderTraversal(root.left, result)
+    postorderTraversal(root.right, result)
+
+    result.push(root.val)
+
+    return result
+};
