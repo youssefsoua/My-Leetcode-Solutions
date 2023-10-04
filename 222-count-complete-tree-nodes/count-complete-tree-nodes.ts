eb3a816364cadd6b4f49b1f67635f786fcd8b4e2
Youssef Soua <youssefsoua94@gmail.com>
2023-10-04 22:55:12 +0100

    Time: 87 ms (74.06%) | Memory: 71.1 MB (42.11%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.ts
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function countNodes(root: TreeNode | null): number {
+    if(!root) return 0;
+
+    const left: number = countNodes(root.left);
+    const right: number = countNodes(root.right);
+
+    return left + right + 1;
+};
