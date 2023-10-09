38bc8ebf9162e72b16d55b555c1489e0a0e4813a
Youssef Soua <youssefsoua94@gmail.com>
2023-10-09 12:36:21 +0100

    Time: 51 ms (87.04%) | Memory: 44.4 MB (57.41%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.ts
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.ts
@@ -28,3 +28,31 @@ function searchRange(nums: number[], target: number): number[] {
 
     return result;
 }
+
+
+
+/*
+function searchRange(nums: number[], target: number): number[] {
+    let left = 0;
+    let right = nums.length - 1;
+    const result = [];
+
+    while (left <= right) {
+        if (nums[left] === target) {
+            result.push(left);
+            break;
+        }
+        left++;
+    }
+
+    while (left <= right) {
+        if (nums[right] === target) {
+            result.push(right);
+            break;
+        }
+        right--;
+    }
+
+    return result.length ? result : [-1, -1];
+}
+*/
