d45439b68d7450f02a7b512a73634b194d3471f1
Youssef Soua <youssefsoua94@gmail.com>
2023-10-09 12:35:35 +0100

    Time: 60 ms (40.19%) | Memory: 43.2 MB (94.81%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.ts
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.ts
@@ -1,23 +1,30 @@
 function searchRange(nums: number[], target: number): number[] {
-    let left = 0;
-    let right = nums.length - 1;
-    const result = [];
+    let low = 0;
+    let high = nums.length - 1;
+    const result = [-1, -1];
 
-    while (left <= right) {
-        if (nums[left] === target) {
-            result.push(left);
-            break;
+    while (low <= high) {
+        const mid = Math.floor((high + low) / 2);
+        if (nums[mid] === target) {
+            let left = mid;
+            while (left >= 0 && nums[left - 1] === target) {
+                left--;
+            }
+            result[0] = left;
+
+            let right = mid;
+            while (right < nums.length && nums[right + 1] === target) {
+                right++;
+            }
+            result[1] = right;
+
+            return result;
+        } else if (nums[mid] > target) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
         }
-        left++;
     }
 
-    while (left <= right) {
-        if (nums[right] === target) {
-            result.push(right);
-            break;
-        }
-        right--;
-    }
-
-    return result.length ? result : [-1, -1];
+    return result;
 }
