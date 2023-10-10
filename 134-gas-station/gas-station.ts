4d97ab17c35bae6b8cf6a8b43902f70d359256a4
Youssef Soua <youssefsoua94@gmail.com>
2023-10-10 16:25:52 +0100

    Time: 86 ms (37.69%) | Memory: 53.9 MB (34.62%) - LeetSync

--- 134-gas-station/gas-station.ts
+++ 134-gas-station/gas-station.ts
@@ -1,15 +1,15 @@
 function canCompleteCircuit(gas: number[], cost: number[]): number {
-    let sumCost = cost.reduce((partialSum, a) => partialSum + a, 0);
-    let sumGas = gas.reduce((partialSum, a) => partialSum + a, 0);
+    let sumCost: number = cost.reduce((partialSum, a) => partialSum + a, 0);
+    let sumGas: number = gas.reduce((partialSum, a) => partialSum + a, 0);
 
     if (sumCost > sumGas) {
         return -1;
     }
 
-    let startIndex = 0;
-    let totalGas = 0;
+    let startIndex: number = 0;
+    let totalGas: number = 0;
 
-    for (let i = 0; i < gas.length; i++) {
+    for (let i: number = 0; i < gas.length; i++) {
         totalGas = totalGas + (gas[i] - cost[i]);
 
         if (totalGas < 0) {
