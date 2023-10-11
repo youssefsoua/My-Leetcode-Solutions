c26fab14e2b3ed70fcc8a16f6de2b15ffac5f10d
Youssef Soua <youssefsoua94@gmail.com>
2023-10-11 16:38:20 +0100

    Time: 111 ms (65.59%) | Memory: 47.9 MB (69.79%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.ts
+++ 13-roman-to-integer/roman-to-integer.ts
@@ -1,4 +1,38 @@
 function romanToInt(s: string): number {
+  const numbers = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  
+  let result = 0;
+  let prevValue = 0;
+
+  for (let i = s.length - 1; i >= 0; i--) {
+    const currentNum = numbers[s[i]];
+
+    if (currentNum < prevValue) {
+      // If the current number is less than the previous one, it's a subtraction case.
+      // Subtract the current number from the result.
+      result -= currentNum;
+    } else {
+      // Otherwise, add the current number to the result.
+      result += currentNum;
+    }
+
+    prevValue = currentNum;
+  }
+
+  return result;
+}
+
+
+/*
+function romanToInt(s: string): number {
     const numbers = {
         I: 1,
         V: 5,
@@ -40,3 +74,4 @@ function romanToInt(s: string): number {
 
     return result;
 }
+*/
