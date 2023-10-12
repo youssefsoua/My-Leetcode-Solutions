712b05bc6758d84ad086f2521815ecc02b169830
Youssef Soua <youssefsoua94@gmail.com>
2023-10-12 21:53:46 +0100

    Time: 120 ms (31.97%) | Memory: 49.5 MB (37.64%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.ts
+++ 12-integer-to-roman/integer-to-roman.ts
@@ -1,4 +1,37 @@
 function intToRoman(num: number): string {
+  const numbers = [
+    { value: 1000, symbol: "M" },
+    { value: 900, symbol: "CM" },
+    { value: 500, symbol: "D" },
+    { value: 400, symbol: "CD" },
+    { value: 100, symbol: "C" },
+    { value: 90, symbol: "XC" },
+    { value: 50, symbol: "L" },
+    { value: 40, symbol: "XL" },
+    { value: 10, symbol: "X" },
+    { value: 9, symbol: "IX" },
+    { value: 5, symbol: "V" },
+    { value: 4, symbol: "IV" },
+    { value: 1, symbol: "I" },
+  ];
+
+  let result = "";
+
+  for (const { value, symbol } of numbers) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
+    }
+  }
+
+  return result;
+}
+
+
+
+/*
+
+function intToRoman(num: number): string {
     const numbers = {
         1: "I",
         4: "IV",
@@ -30,10 +63,12 @@ function intToRoman(num: number): string {
         } else {
             result += numbers[multiplier * val];
         }
-        
+
         number %= multiplier;
         multiplier /= 10;
     }
 
     return result;
 }
+
+*/
