bd399d481c0f820a6f66ecb6750f81df609ce1fb
Youssef Soua <youssefsoua94@gmail.com>
2023-10-14 14:26:49 +0100

    Time: 62 ms (24.76%) | Memory: 45 MB (11.24%) - LeetSync

---
+++ 48-rotate-image/rotate-image.ts
@@ -0,0 +1,25 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function rotate(matrix: number[][]): void {
+    const n = matrix.length;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j < n; j++) {
+            if (i !== j)
+                [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        let left = 0;
+        let right = n - 1;
+
+        while (left < right) {
+            [matrix[i][left++], matrix[i][right--]] = [
+                matrix[i][right],
+                matrix[i][left],
+            ];
+        }
+    }
+}
