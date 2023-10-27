75ded543ba30fbb21c0a6541ee5b986ba374dcfc
Youssef Soua <youssefsoua94@gmail.com>
2023-10-27 09:08:57 +0100

    Time: 321 ms (44.87%) | Memory: 87.7 MB (13.19%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.ts
+++ 5-longest-palindromic-substring/longest-palindromic-substring.ts
@@ -1,25 +1,40 @@
 function longestPalindrome(s: string): string {
-  if (s.length < 2) return s;
+    if (s.length === 0) return "";
 
-  let longest = "";
+    const n = s.length;
+    const dp: boolean[][] = new Array(n).fill(0).map(() => new Array(n).fill(false));
+    let start = 0;
+    let maxLen = 1;
 
-  for (let i = 0; i < s.length; i++) {
-    for (let j = s.length - 1; j >= i + longest.length; j--) {
-      if (s[i] === s[j]) {
-        const substr = s.slice(i, j + 1);
-        if (isPalindrome(substr) && substr.length > longest.length) {
-          longest = substr;
-          break;
-        }
-      }
+    // All characters are palindromes of length 1
+    for (let i = 0; i < n; i++) {
+        dp[i][i] = true;
     }
-  }
 
-  return longest === "" ? s[0] : longest;
-}
+    // Check for palindromes of length 2
+    for (let i = 0; i < n - 1; i++) {
+        if (s[i] === s[i + 1]) {
+            dp[i][i + 1] = true;
+            start = i;
+            maxLen = 2;
+        }
+    }
 
-function isPalindrome(s: string): boolean {
-  return s === s.split('').reverse().join('');
+    // Check for palindromes of length 3 or more
+    for (let len = 3; len <= n; len++) {
+        for (let i = 0; i <= n - len; i++) {
+            const j = i + len - 1;
+            if (dp[i + 1][j - 1] && s[i] === s[j]) {
+                dp[i][j] = true;
+                if (len > maxLen) {
+                    start = i;
+                    maxLen = len;
+                }
+            }
+        }
+    }
+
+    return s.substring(start, start + maxLen);
 }
 
 //Timeout recursive with memo
