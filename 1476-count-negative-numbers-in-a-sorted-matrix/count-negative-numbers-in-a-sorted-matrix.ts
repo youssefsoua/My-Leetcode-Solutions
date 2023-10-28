5a85e31233b9dc32ce1577487f122c8c68c0f74c
Youssef Soua <youssefsoua94@gmail.com>
2023-10-28 09:19:00 +0100

    Time: 60 ms (43.86%) | Memory: 44.7 MB (84.21%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.ts
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.ts
@@ -25,3 +25,38 @@ function countNegatives(grid: number[][]): number {
     }
     return count;
 }
+
+/**
+You can optimize the given code for counting negatives in a 2D grid by taking advantage of the properties of the grid. Here's an optimized version of the code:
+
+```typescript
+function countNegatives(grid: number[][]): number {
+    const m = grid.length;
+    const n = grid[0].length;
+    let count = 0;
+    let i = 0;
+    let j = n - 1;
+
+    while (i < m && j >= 0) {
+        if (grid[i][j] < 0) {
+            count += m - i; // Count all negatives in the current column.
+            j--; // Move to the previous column.
+        } else {
+            i++; // Move to the next row.
+        }
+    }
+
+    return count;
+}
+```
+
+Here are the optimizations made in this code:
+
+1. Instead of starting from the last row and last column, we start from the first row and last column. This way, we can traverse the grid in a more natural order, which is more efficient for counting negatives.
+
+2. We iterate through the rows and columns while incrementing `i` and decrementing `j`. This ensures that we always move to the next row or the previous column, depending on whether the current element is positive or negative. This eliminates the need for binary search within each row.
+
+3. We count all the negatives in the current column by adding `m - i` to the `count` variable, where `m - i` represents the number of rows below the current row.
+
+This optimized code has a time complexity of O(m + n), where `m` is the number of rows, and `n` is the number of columns, as it iterates through the grid only once. It is more efficient than the binary search approach and should perform better for most inputs.
+ */
