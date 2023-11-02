159d199b7220beaa7af568f4a738e9f29bb05af4
Youssef Soua <youssefsoua94@gmail.com>
2023-11-02 01:37:07 +0100

    Time: 70 ms (42.42%) | Memory: 48.9 MB (15.15%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.ts
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.ts
@@ -12,21 +12,35 @@
  * }
  */
 
-function averageOfSubtree(root: TreeNode | null): number {
-    let result = 0;
 
-    const getAverage = (root: TreeNode | null): number[] => {
-        if (!root) return [0, 0];
-        const [leftNodesNumber, leftNodesSum] = getAverage(root.left);
-        const [rightNodesNumber, rightNodesSum] = getAverage(root.right);
-        const currentNodesNumber = leftNodesNumber + rightNodesNumber + 1;
-        const currentNodesSum = leftNodesSum + rightNodesSum + root.val;
-        if (Math.floor(currentNodesSum / currentNodesNumber) === root.val)
-            result++;
-        return [currentNodesNumber, currentNodesSum];
+function averageOfSubtree(root: TreeNode | null): number {
+    let result = 0; // Initialize the result counter.
+
+    // Helper function to recursively calculate averages of subtrees.
+    const getAverage = (node: TreeNode | null): [number, number] => {
+        if (!node) {
+            // Base case: Return [0, 0] for an empty node.
+            return [0, 0];
+        }
+
+        // Recursively calculate left and right subtree values.
+        const [leftNodes, leftSum] = getAverage(node.left);
+        const [rightNodes, rightSum] = getAverage(node.right);
+
+        // Calculate the total nodes and total sum for the current subtree.
+        const totalNodes = leftNodes + rightNodes + 1;
+        const totalSum = leftSum + rightSum + node.val;
+
+        // Check if the average of the subtree is equal to the node's value.
+        if (Math.floor(totalSum / totalNodes ) === node.val) {
+            result++; // Increment the result counter.
+        }
+
+        return [totalNodes, totalSum]; // Return total nodes and sum for the current subtree.
     };
 
+    // Start the recursive calculation with the root node.
     getAverage(root);
 
-    return result;
+    return result; // Return the final result.
 }
