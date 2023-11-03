ad21ff00e1716bf0e7f49dd9131963843d56eb37
Youssef Soua <youssefsoua94@gmail.com>
2023-11-03 11:03:31 +0100

    Time: 60 ms (28.57%) | Memory: 44.3 MB (28.57%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.ts
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.ts
@@ -1,18 +1,21 @@
 function buildArray(target: number[], n: number): string[] {
-   let index: number = 0;
-   let current: number = 1;
-   const stack: number[] = [];
-   const operations: string[] = [];
+   let index: number = 0; // Initialize an index to keep track of the target array elements.
+   let current: number = 1; // Initialize a current number to simulate the stream of integers.
+   const stack: number[] = []; // Initialize an empty stack to simulate the stack operations.
+   const operations: string[] = []; // Initialize an array to store the operations performed.
+
    while (index < target.length) {
-      stack.push(current);
-      operations.push("Push");
+      stack.push(current); // Push the current number to the stack.
+      operations.push("Push"); // Add "Push" operation to the operations array.
+
       if (target[index] === stack[index]) {
-         index++;
+         index++; // If the stack matches the target, move to the next target element.
       } else {
-         stack.pop();
-         operations.push("Pop");
+         stack.pop(); // If not, pop the top element from the stack.
+         operations.push("Pop"); // Add "Pop" operation to the operations array.
       }
-      current++;
+      current++; // Move to the next number in the stream.
    }
-   return operations;
+
+   return operations; // Return the array of operations.
 }
