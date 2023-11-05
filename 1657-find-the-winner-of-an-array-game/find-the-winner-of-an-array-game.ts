941103fc048b6702ef08116938144da53fc12313
Youssef Soua <youssefsoua94@gmail.com>
2023-11-05 01:58:03 +0100

    Time: 80 ms (33.33%) | Memory: 52.9 MB (33.33%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.ts
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.ts
@@ -1,44 +1,32 @@
 function getWinner(arr: number[], k: number): number {
-    let i = 0;
-    let j = 1;
-    const n = arr.length;
-    const counter = new Map();
+    const counter = new Map<number, number>();
     let max = 0;
 
-    while (i<n && j<n) {
-        max = Math.max(arr[i], arr[j])
-         if (arr[i] > arr[j]) {
-            const count = counter.get(arr[i]);
-            if(count === undefined) counter.set(arr[i],1)
-            else {
-                counter.set(arr[i], count + 1);
-            }
+    for (let i = 0, j = 1; i < arr.length && j < arr.length;) {
+        const current = arr[i] > arr[j] ? arr[i] : arr[j];
+        const count = counter.get(current) || 0;
+        counter.set(current, count + 1);
 
-            if (counter.get(arr[i]) === k) {
-                return arr[i];
-            }
+        if (counter.get(current) === k) {
+            return current;
+        }
 
+        if (arr[i] > arr[j]) {
             if (i > j) {
                 j = i + 1;
             } else {
                 j++;
             }
         } else {
-            const count = counter.get(arr[j]);
-            if(count === undefined) counter.set(arr[j],1)
-            else {
-                counter.set(arr[j], count + 1);
-            }
-            if (counter.get(arr[j]) === k) {
-                return arr[j];
-            }
-
             if (j > i) {
                 i = j + 1;
             } else {
                 i++;
             }
         }
+
+        max = Math.max(max, current);
     }
-    return max
+
+    return max;
 }
