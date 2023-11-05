b6a3e4fa862f52e338eeeb0753a0b5492d3add25
Youssef Soua <youssefsoua94@gmail.com>
2023-11-05 01:44:50 +0100

    Time: 106 ms (33.33%) | Memory: 73 MB (33.33%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.ts
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.ts
@@ -1,35 +1,45 @@
 function getWinner(arr: number[], k: number): number {
-    let i = 0
-    let j = 1
-    const n = arr.length
-    const counter = new Map()
-    let max = 0
-    for(const e of arr){
-        counter.set(e,0)
-        max = Math.max(max,e)
+    let i = 0;
+    let j = 1;
+    const n = arr.length;
+    const counter = new Map();
+    let max = 0;
+
+    for (const e of arr) {
+        counter.set(e, 0);
+        max = Math.max(max, e);
     }
 
-    if(k>n) return max
+    if (k > n) return max;
 
-    while(true){
-        let tempI = i%n
-        let tempJ = j%n
+    while (true) {
+        if (arr[i] === max || arr[j] === max) return max;
+        if (arr[i] > arr[j]) {
+            const count = counter.get(arr[i]);
+            if (count + 1 === k) {
+                return arr[i];
+            } else {
+                counter.set(arr[i], count + 1);
+            }
 
-        if(arr[tempI]>arr[tempJ]){
-            const count = counter.get(arr[tempI])
-            if(count + 1 === k) return arr[tempI]
-            else counter.set(arr[tempI], count + 1)
+            if (i > j) {
+                j = i + 1;
+            } else {
+                j++;
+            }
+        } else {
+            const count = counter.get(arr[j]);
+            if (count + 1 === k) {
+                return arr[j];
+            } else {
+                counter.set(arr[j], count + 1);
+            }
 
-            if(i>j && tempI>tempJ) j = i+1
-            else j++
-        }else{
-            const count = counter.get(arr[tempJ])
-            if(count + 1 === k) return arr[tempJ]
-            else counter.set(arr[tempJ], count + 1)
-
-            if(j>i && tempJ>tempI) i = j+1
-            else i++
+            if (j > i) {
+                i = j + 1;
+            } else {
+                i++;
+            }
         }
     }
-   
-};
+}
