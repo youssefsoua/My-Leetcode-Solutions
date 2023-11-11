d18b90c25294e227a78e94fb48027041557e2b81
Youssef Soua <youssefsoua94@gmail.com>
2023-11-11 21:06:58 +0100

    Time: 56 ms (85.05%) | Memory: 45.1 MB (42.19%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.ts
@@ -0,0 +1,20 @@
+function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
+    const map = new Map<number, number>();
+    const stack: number[] = [];
+    const res: number[] = [];
+
+    for (let i = nums2.length - 1; i >= 0; i--) {
+        while (stack.length && nums2[i] > nums2[stack[stack.length - 1]]) {
+            stack.pop();
+        }
+
+        if (stack.length) map.set(nums2[i], nums2[stack[stack.length - 1]]);
+        else map.set(nums2[i], -1);
+
+        stack.push(i);
+    }
+    for (const num of nums1) {
+        res.push(map.get(num));
+    }
+    return res;
+}
