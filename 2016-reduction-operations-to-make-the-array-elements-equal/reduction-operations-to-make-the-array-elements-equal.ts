39dcd10707eea57ce0fc1f46f9e37922e13c2988
Youssef Soua <youssefsoua94@gmail.com>
2023-11-19 02:17:23 +0100

    Time: 181 ms (100.00%) | Memory: 59.3 MB (33.33%) - LeetSync

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.ts
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.ts
@@ -1,22 +1,22 @@
 function reductionOperations(nums: number[]): number {
     const n: number = nums.length;
 
-    if (n === 1) return 0;
+    if (n === 1) return 0; // If the array has only one element, no reduction operations are needed.
 
-    nums.sort((a, b) => b - a);
+    nums.sort((a, b) => b - a); // Sort the array in descending order.
 
     let last = nums.length - 1;
     while (nums[last] === nums[last - 1]) {
-        last--;
+        last--; // Find the index of the last distinct element in the sorted array.
     }
 
     let count: number = 0;
     for (let i = 0; i < last; i++) {
         while (i < last && nums[i] === nums[i + 1]) {
-            i++;
+            i++; // Skip duplicate elements in the sorted array.
         }
-        count += (i + 1);
+        count += (i + 1); // Add the index (plus one) of each distinct element to the count.
     }
 
-    return count;
+    return count; // Return the total count of reduction operations.
 }
