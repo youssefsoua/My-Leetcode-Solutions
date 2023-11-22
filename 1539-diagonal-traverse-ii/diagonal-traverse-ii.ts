8140ea8036d69edff594c5da38874812b70ac420
Youssef Soua <youssefsoua94@gmail.com>
2023-11-22 11:34:57 +0100

    Time: 201 ms (94.12%) | Memory: 106.3 MB (52.94%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.ts
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.ts
@@ -1,21 +1,35 @@
 function findDiagonalOrder(nums: number[][]): number[] {
-    const groups: { [key: number]: number[] } = {};
+    // Map to store groups of elements based on diagonal position
+    const groups = new Map<number, number[]>();
 
+    // Iterate through the 2D array from bottom-left to top-right
     for (let row = nums.length - 1; row >= 0; row--) {
         for (let col = 0; col < nums[row].length; col++) {
+            // Calculate the diagonal position for the current element
             const diagonal = row + col;
-            groups[diagonal] = groups[diagonal] || [];
-            groups[diagonal].push(nums[row][col]);
+
+            // Retrieve or create a group for the current diagonal
+            const group = groups.get(diagonal);
+            if (group) {
+                // If the group exists, add the current element to it
+                group.push(nums[row][col]);
+            } else {
+                // If the group does not exist, create a new group with the current element
+                groups.set(diagonal, [nums[row][col]]);
+            }
         }
     }
 
+    // Array to store the final result in diagonal order
     const ans: number[] = [];
     let curr = 0;
 
-    while (groups[curr] !== undefined) {
-        ans.push(...groups[curr]);
+    // Concatenate the elements from each diagonal group to the result array
+    while (groups.has(curr)) {
+        ans.push(...groups.get(curr));
         curr++;
     }
 
+    // Return the final array containing elements in diagonal order
     return ans;
 }
