e70cd5e9d4818d7a1e342d8280259cdf7f25890e
Youssef Soua <youssefsoua94@gmail.com>
2023-11-23 10:08:17 +0100

    Time: 129 ms (54.55%) | Memory: 49.8 MB (54.55%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.ts
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.ts
@@ -3,19 +3,22 @@ function checkArithmeticSubarrays(
     l: number[],
     r: number[],
 ): boolean[] {
-    
+
     const result: boolean[] = [];
 
     for (let i = 0; i < l.length; i++) {
+
         const temp: number[] = nums.slice(l[i], r[i] + 1).sort((a, b) => a - b);
-        let index = 1;
-        const diff = temp[1] - temp[0];
+        let index: number = 1;
+
+        const diff: number = temp[1] - temp[0];
 
         while (index < temp.length) {
             if (temp[index] - temp[index - 1] !== diff) {
                 result.push(false);
                 break;
             }
+            
             if (index === temp.length - 1) result.push(true);
 
             index++;
