744e3a74f8e62221a9cec4fd93e3ef8777bf0ea0
Youssef Soua <youssefsoua94@gmail.com>
2023-11-25 13:11:09 +0100

    Time: 171 ms (100.00%) | Memory: 79.7 MB (28.57%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.ts
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.ts
@@ -1,17 +1,13 @@
 function getSumAbsoluteDifferences(nums: number[]): number[] {
     const n = nums.length;
-    const prefix: number[] = [];
-    const suffix: number[] = [];
-    const result: number[] = [];
+    const result: number[] = new Array(n);
+    let prefixSum = 0;
+    let suffixSum = nums.reduce((acc, num) => acc + num, 0);
 
     for (let i = 0; i < n; i++) {
-        prefix[i] = (nums[i - 1] || 0) + (prefix[i - 1] || 0);
-        suffix[n - 1 - i] = (nums[n - i] || 0) + (suffix[n - i] || 0);
-    }
-
-    for (let i = 0; i < n; i++) {
-        result[i] =
-            nums[i] * i - prefix[i] + (suffix[i] - nums[i] * (n - i - 1));
+        result[i] = nums[i] * i - prefixSum + (suffixSum - nums[i] * (n - i));
+        prefixSum += nums[i];
+        suffixSum -= nums[i];
     }
 
     return result;
