e0d1a690be8344e04f0ea996d5af5479ca5a4ca8
Youssef Soua <youssefsoua94@gmail.com>
2023-11-25 13:17:00 +0100

    Time: 175 ms (100.00%) | Memory: 79 MB (28.57%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.ts
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.ts
@@ -1,6 +1,7 @@
 function getSumAbsoluteDifferences(nums: number[]): number[] {
     const n = nums.length;
     const result: number[] = new Array(n);
+
     let prefixSum = 0;
     let suffixSum = nums.reduce((acc, num) => acc + num, 0);
 
@@ -12,3 +13,26 @@ function getSumAbsoluteDifferences(nums: number[]): number[] {
 
     return result;
 }
+
+/**
+My Solution:
+
+function getSumAbsoluteDifferences(nums: number[]): number[] {
+    const n = nums.length;
+    const prefix: number[] = [];
+    const suffix: number[] = [];
+    const result: number[] = [];
+
+    for (let i = 0; i < n; i++) {
+        prefix[i] = (nums[i - 1] || 0) + (prefix[i - 1] || 0);
+        suffix[n - 1 - i] = (nums[n - i] || 0) + (suffix[n - i] || 0);
+    }
+
+    for (let i = 0; i < n; i++) {
+        result[i] =
+            nums[i] * i - prefix[i] + (suffix[i] - nums[i] * (n - i - 1));
+    }
+
+    return result;
+}
+ */
