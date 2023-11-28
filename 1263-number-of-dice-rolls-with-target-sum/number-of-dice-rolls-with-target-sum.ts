c222ae4ae987992e9ce91df8a5a473825241a6a7
Youssef Soua <youssefsoua94@gmail.com>
2023-11-28 10:13:46 +0100

    Time: 208 ms (36.00%) | Memory: 52 MB (48.00%) - LeetSync

---
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.ts
@@ -0,0 +1,19 @@
+function numRollsToTarget(n: number, k: number, target: number, memo: Record<string, number> = {}): number {
+        if (n === 0 && target === 0) return 1;
+        if (n === 0 || target <= 0) return 0;
+
+        const memoKey = `${n}-${target}`;
+        if (memo[memoKey] !== undefined) {
+            return memo[memoKey];
+        }
+
+        let result = 0;
+        for (let i = 1; i <= k; i++) {
+            result += numRollsToTarget(n - 1, k, target - i, memo);
+            result %= 1000000007; // Add modulo to handle large results as per the problem constraints
+        }
+
+        memo[memoKey] = result;
+        return result;
+
+}
