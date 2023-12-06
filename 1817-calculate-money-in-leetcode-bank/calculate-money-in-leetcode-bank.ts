9b48313ea1d52a77789bb10e479aa611b3a99b36
Youssef Soua <youssefsoua94@gmail.com>
2023-12-06 12:45:37 +0100

    Time: 45 ms (100.00%) | Memory: 42.8 MB (58.33%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.ts
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.ts
@@ -1,14 +1,13 @@
 function totalMoney(n: number): number {
     const rest: number = n % 7;
 
-    const multiplier: number = rest? Math.floor(n / 7) + 1 : Math.floor(n / 7);
-    
+    const multiplier: number = rest ? Math.floor(n / 7) + 1 : Math.floor(n / 7);
+
     let total: number = 28 * multiplier;
 
     for (let i = 1; i < multiplier; i++) {
         total += i * 7;
     }
-    console.log(rest, total, multiplier)
 
     if (rest) {
         for (let i = 7; i > rest; i--) {
