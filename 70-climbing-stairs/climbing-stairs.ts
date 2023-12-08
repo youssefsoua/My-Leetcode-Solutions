49dfd32ad94fd938cc0ae0964c88d6741eb27554
Youssef Soua <youssefsoua94@gmail.com>
2023-12-08 09:59:38 +0100

    Time: 52 ms (68.31%) | Memory: 42.8 MB (43.61%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.ts
+++ 70-climbing-stairs/climbing-stairs.ts
@@ -1,3 +1,15 @@
+function climbStairs(n: number): number {
+    const dp: number[] = [0, 1, 2];
+
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+}
+
+/*
+
 function climbStairs(n: number, cache: Record<number, number> = {}): number {
     if (cache[n] !== undefined) {
         return cache[n];
@@ -14,3 +26,5 @@ function climbStairs(n: number, cache: Record<number, number> = {}): number {
     cache[n] = climbStairs(n - 1, cache) + climbStairs(n - 2, cache);
     return cache[n];
 }
+
+ */
