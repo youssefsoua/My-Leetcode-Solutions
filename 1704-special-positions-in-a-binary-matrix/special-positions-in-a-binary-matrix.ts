12db999ef6c492dc91fb03a8dd811aaa988ca9e2
Youssef Soua <youssefsoua94@gmail.com>
2023-12-13 16:55:54 +0100

    Time: 65 ms (50.00%) | Memory: 45.4 MB (50.00%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.ts
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.ts
@@ -1,4 +1,40 @@
 function numSpecial(mat: number[][]): number {
+    const m: number = mat.length;
+    const n: number = mat[0].length;
+    let count: number = 0;
+
+    for (let i = 0; i < m; i++) {
+        let columnIdx: number = -1;
+
+        for (let j = 0; j < n; j++) {
+            if (mat[i][j] === 1) {
+                if (columnIdx === -1) {
+                    columnIdx = j;
+                } else {
+                    columnIdx = -1; // More than one '1' in the row, reset columnIdx
+                    break;
+                }
+            }
+        }
+
+        if (columnIdx !== -1) {
+            let uniqueCount = 0;
+            for (let k = 0; k < m; k++) {
+                if (mat[k][columnIdx] === 1) {
+                    uniqueCount++;
+                    if (uniqueCount > 1) break;
+                }
+            }
+            if (uniqueCount === 1) count++;
+        }
+    }
+
+    return count;
+}
+
+/*
+
+function numSpecial(mat: number[][]): number {
     const special: number[][] = [];
     const m: number = mat.length;
     const n: number = mat[0].length;
@@ -33,3 +69,4 @@ function numSpecial(mat: number[][]): number {
 
     return count;
 }
+*/
