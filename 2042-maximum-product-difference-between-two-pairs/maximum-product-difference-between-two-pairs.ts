ba18df464b3a78438bf83c8c309340dcd4f921e2
Youssef Soua <youssefsoua94@gmail.com>
2023-12-18 09:47:20 +0100

    Time: 67 ms (86.44%) | Memory: 46 MB (61.02%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.ts
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.ts
@@ -1,38 +1,24 @@
 function maxProductDifference(nums: number[]): number {
-    let min1: number = 0;
-    let min2: number = 0;
-    let max1: number = 0;
-    let max2: number = 0;
+    let biggest = 0;
+    let secondBiggest = 0;
+    let smallest = Infinity;
+    let secondSmallest = Infinity;
 
-    let min: number = Infinity;
-    let max: number = 0;
-
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] < min) {
-            min1 = i;
-            min = nums[i];
+    for (const num of nums) {
+        if (num > biggest) {
+            secondBiggest = biggest;
+            biggest = num;
+        } else if (num > secondBiggest) {
+            secondBiggest = num;
         }
 
-        if (nums[i] > max) {
-            max1 = i;
-            max = nums[i];
-        }
-    }
-
-    min = Infinity;
-    max = 0;
-
-    for (let i = 0; i < nums.length; i++) {
-        if (i !== min1 && nums[i] < min) {
-            min2 = i;
-            min = nums[i];
-        }
-
-        if (i !== max1 && nums[i] > max) {
-            max2 = i;
-            max = nums[i];
+        if (num < smallest) {
+            secondSmallest = smallest;
+            smallest = num;
+        } else if (num < secondSmallest) {
+            secondSmallest = num;
         }
     }
 
-    return nums[max1] * nums[max2] - nums[min1] * nums[min2];
+    return biggest * secondBiggest - smallest * secondSmallest;
 }
