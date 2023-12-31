a6a1ad81fefbf4350652063ba27f2c6abfa83613
Youssef Soua <youssefsoua94@gmail.com>
2023-12-31 01:34:01 +0100

    Time: 230 ms (70.39%) | Memory: 47.7 MB (80.26%) - LeetSync

--- 1637-string-compression-ii/string-compression-ii.ts
+++ 1637-string-compression-ii/string-compression-ii.ts
@@ -1,15 +1,35 @@
 function getLengthOfOptimalCompression(s: string, k: number): number {
+    // Helper function to calculate the length of the compressed string segment
     const getLength = (cnt: number) => cnt === 1 ? 1 : cnt < 10 ? 2 : cnt < 100 ? 3 : 4;
-    const n = s.length, dp = Array.from({ length: n + 1 }, () => Array(k + 1));
-    for (let i = n; i >= 0; i--)
+
+    const n = s.length;
+    // Initialize a 2D array dp where dp[i][j] represents the minimum length of the compressed string for the first i characters after deleting j characters
+    const dp = Array.from({ length: n + 1 }, () => Array(k + 1));
+
+    // Iterate over the string in reverse order
+    for (let i = n; i >= 0; i--) {
         for (let j = 0; j <= k; j++) {
-            if (i === n) { dp[n][j] = 0; continue; }
+            if (i === n) { 
+                // Base case: if there are no characters left, the length of the compressed string is 0
+                dp[n][j] = 0; 
+                continue; 
+            }
+            // If we can delete a character, the minimum length is either the current minimum length or the minimum length after deleting the current character
             dp[i][j] = j > 0 ? dp[i + 1][j - 1] : Infinity;
+
             let del = j, cnt = 0;
-            for (let iEnd = i; iEnd < n && del >= 0; iEnd++)
-                if (s[iEnd] === s[i])
-                    dp[i][j] = Math.min(dp[i][j], getLength(++cnt) + dp[iEnd + 1][del])
-                else del--;
+            // Iterate over the rest of the string to find segments of the same character
+            for (let iEnd = i; iEnd < n && del >= 0; iEnd++) {
+                if (s[iEnd] === s[i]) {
+                    // If the current character is the same as the start of the segment, increment the count and update the minimum length
+                    dp[i][j] = Math.min(dp[i][j], getLength(++cnt) + dp[iEnd + 1][del]);
+                } else {
+                    // If the current character is different, we can delete it and decrement the number of deletions left
+                    del--;
+                }
+            }
         }
+    }
+    // Return the minimum length of the compressed string after deleting k characters
     return dp[0][k];
-};
+}
