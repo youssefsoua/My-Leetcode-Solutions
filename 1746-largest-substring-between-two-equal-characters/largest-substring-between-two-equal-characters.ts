4a7f614141a76f4909c6c624bed01f178abe435e
Youssef Soua <youssefsoua94@gmail.com>
2023-12-31 01:15:48 +0100

    Time: 95 ms (7.14%) | Memory: 42.4 MB (100.00%) - LeetSync

---
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.ts
@@ -0,0 +1,25 @@
+function maxLengthBetweenEqualCharacters(s: string): number {
+    // Create a map to store the last index of each character
+    const indexes = new Map<string, number>();
+    
+    // Initialize the maximum length variable
+    let max: number = -1;
+
+    // Iterate through each character in the string
+    for (let i = 0; i < s.length; i++) {
+        // Retrieve the last index of the current character from the map
+        const index = indexes.get(s[i]);
+
+        // If the character is found in the map, update the maximum length
+        if (index !== undefined) {
+            max = Math.max(max, i - index - 1);
+        }else{
+            // Set the current index of the character in the map
+            indexes.set(s[i], i);
+        }
+
+    }
+
+    // Return the maximum length between equal characters
+    return max;
+}
