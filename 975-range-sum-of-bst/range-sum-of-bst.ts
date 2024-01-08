01449d7a31aac0210fb3ac8cc6e34a4832e0e074
Youssef Soua <youssefsoua94@gmail.com>
2024-01-08 20:51:32 +0100

    Time: 142 ms (23.77%) | Memory: 98 MB (16.59%) - LeetSync

---
+++ 975-range-sum-of-bst/range-sum-of-bst.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function rangeSumBST(root: TreeNode | null, low: number, high: number): number {
+    if (!root) return 0;
+
+    let sum: number = 0;
+
+    sum += rangeSumBST(root.left, low, high);
+    sum += rangeSumBST(root.right, low, high);
+
+    if (root.val >= low && root.val <= high) sum += root.val;
+
+    return sum;
+}
