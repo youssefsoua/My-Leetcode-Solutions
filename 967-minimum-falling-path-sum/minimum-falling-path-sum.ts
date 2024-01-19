fcddbe8f0d25406a9b82a9e2d61f02cebff7c251
Youssef Soua <youssefsoua94@gmail.com>
2024-01-19 09:35:48 +0100

    Time: 71 ms (45.71%) | Memory: 44.7 MB (94.29%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.ts
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.ts
@@ -1,9 +1,18 @@
 function minFallingPathSum(matrix: number[][]): number {
     const n: number = matrix.length;
+
+    if (n === 1) return matrix[0][0];
+
+    let min: number = Infinity;
+
     for (let i = 1; i < n; i++) {
         for (let j = 0; j < n; j++) {
+
             matrix[i][j] += Math.min(matrix[i - 1][j], (matrix[i - 1][j - 1] || Infinity), (matrix[i - 1][j + 1] || Infinity));
+
+            if (i === n - 1) min = Math.min(min, matrix[i][j]);
         }
     }
-    return Math.min(...matrix[n - 1]);
+
+    return min;
 };
