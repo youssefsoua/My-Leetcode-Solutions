a91b507d2ff0ecb894eee737fc7665a755273c13
Youssef Soua <youssefsoua94@gmail.com>
2024-01-23 20:09:47 +0100

    Time: 104 ms (41.67%) | Memory: 54 MB (16.67%) - LeetSync

---
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.ts
@@ -0,0 +1,48 @@
+function maxLength(arr: string[]): number {
+    const words: number[][] = []
+    let max: number = 0
+    const n: number = arr.length
+
+    for (const str of arr) {
+        const word: number[] = new Array(26).fill(0)
+        for (const char of str) {
+            word[char.charCodeAt(0) - 97]++
+        }
+        words.push(word)
+    }
+
+    const backtrack = (word: number[], index: number, sum: number) => {
+        if (index < n) {
+            for (let i = index; i < n; i++) {
+                let isUnique: boolean = true
+
+                for (let j = 0; j < 26; j++) {
+                    if (word[j] + words[i][j] > 1) {
+                        isUnique = false
+                        break
+                    }
+                }
+
+                if (isUnique) {
+                    sum += arr[i].length
+                    max = Math.max(max, sum)
+
+                    for (let j = 0; j < 26; j++) {
+                        word[j] += words[i][j]
+                    }
+                    backtrack(word, i + 1, sum)
+                    sum -= arr[i].length
+                    for (let j = 0; j < 26; j++) {
+                        word[j] -= words[i][j]
+                    }
+                }
+                isUnique = true
+            }
+        }
+    }
+
+    const word: number[] = new Array(26).fill(0)
+    backtrack(word, 0, 0)
+
+    return max
+};
