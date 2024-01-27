a9dc7cc464b30d1d1a48d3616f774b0aa7973707
Youssef Soua <youssefsoua94@gmail.com>
2024-01-27 15:46:34 +0100

    Time: 60 ms (69.14%) | Memory: 53.5 MB (6.17%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.ts
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.ts
@@ -1,21 +1,21 @@
 function kWeakestRows(mat: number[][], k: number): number[] {
-    const counter = new Map<number, number>()
-    const result: number[] = []
+    const counter = new Map<number, number>();
+    const result: number[] = [];
 
-    for(let i = 0; i<mat.length; i++){
-        let sum: number = 0
-        for(const e of mat[i]){
-            if(e) sum++
+    for (let i = 0; i < mat.length; i++) {
+        let sum: number = 0;
+        for (const e of mat[i]) {
+            if (e) sum++;
         }
-        counter.set(i, sum)
+        counter.set(i, sum);
     }
 
-    const temp = Array.from(counter.entries()).sort((a,b)=>a[1]-b[1])
+    const temp = Array.from(counter.entries()).sort((a, b) => a[1] - b[1]);
 
-    for(let i = 0; i<k; i++){
-        result.push(temp[i][0])
+    for (let i = 0; i < k; i++) {
+        result.push(temp[i][0]);
     }
-    
-    return result
+
+    return result;
 
 };
