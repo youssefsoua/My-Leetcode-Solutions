ec7faa0047917ce957c0ce131c1d42d660667e5c
Youssef Soua <youssefsoua94@gmail.com>
2024-02-26 13:47:36 +0100

    Time: 65 ms (53.17%) | Memory: 52.1 MB (41.72%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.ts
@@ -0,0 +1,25 @@
+type Counter = {
+    increment: () => number,
+    decrement: () => number,
+    reset: () => number,
+}
+
+function createCounter(init: number): Counter {
+    let value = init;
+    return {
+        increment: () => ++value,
+        decrement: () => --value,
+        reset: () => {
+            value = init
+            return value
+        },
+    };
+
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
