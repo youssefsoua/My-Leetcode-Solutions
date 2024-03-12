4d610d51679b111379d994c7966d2741add30d1e
Youssef Soua <youssefsoua94@gmail.com>
2024-03-12 05:35:40 +0100

    Time: 71 ms (40.00%) | Memory: 52.3 MB (40.00%) - LeetSync

---
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.ts
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeZeroSumSublists(head: ListNode | null): ListNode | null {
+    let dummy: ListNode = new ListNode(0);
+    dummy.next = head;
+    let sum: number = 0;
+    let map: Map<number, ListNode> = new Map();
+    map.set(0, dummy);
+
+    while (head) {
+        sum += head.val;
+        if (map.has(sum)) {
+            let prev: ListNode = map.get(sum)!;
+            let tempSum: number = sum;
+            let temp: ListNode | null = prev.next;
+            while (temp !== head) {
+                tempSum += temp!.val;
+                map.delete(tempSum);
+                temp = temp!.next;
+            }
+            prev.next = head.next;
+        } else {
+            map.set(sum, head);
+        }
+        head = head.next;
+    }
+
+    return dummy.next;
+}
