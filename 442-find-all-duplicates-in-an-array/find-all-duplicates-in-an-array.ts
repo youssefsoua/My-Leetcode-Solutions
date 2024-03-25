9dd1fb4c397027ed95d026b62d1f94b815282af2
Youssef Soua <youssefsoua94@gmail.com>
2024-03-25 05:00:09 +0100

    Time: 88 ms (63.44%) | Memory: 68.6 MB (15.05%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.ts
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.ts
@@ -1,10 +1,11 @@
 function findDuplicates(nums: number[]): number[] {
     const set = new Set<number>(nums);
+    const result: number[] = [];
 
     for (const num of nums) {
         if (set.has(num)) set.delete(num);
-        else set.add(num);
+        else result.push(num);
     }
 
-    return Array.from(set);
+    return result;
 };
