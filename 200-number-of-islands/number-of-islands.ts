7a273da175819532bb5a244e5133fd883ed53cb1
Youssef Soua <youssefsoua94@gmail.com>
2024-04-19 10:58:53 +0100

    Time: 92 ms (38.19%) | Memory: 55.1 MB (61.38%) - LeetSync

--- 200-number-of-islands/number-of-islands.ts
+++ 200-number-of-islands/number-of-islands.ts
@@ -1,27 +1,27 @@
 function numIslands(grid: string[][]): number {
-    const m = grid.length;
-    const n = grid[0].length;
-    let count = 0;
-    const directions = [[1,0],[-1,0],[0,1],[0,-1]];
+    const numRows = grid.length;
+    const numCols = grid[0].length;
+    let islandCount = 0;
+    const directions: [number, number][] = [[1, 0], [-1, 0], [0, 1], [0, -1]];
 
-    for(let i = 0; i < m; i++){
-        for(let j = 0; j < n; j++){
-            if(grid[i][j] === "1"){
-                const stack = [[i,j]];
-                while(stack.length){
-                    const [x,y] = stack.pop();
-                    grid[x][y] = "0";
-                    for(const direction of directions){
-                        const [v,w] = direction;
-                        const newX = v+x;
-                        const newY = w+y;
-                        if(newX >= 0 && newX < m && newY >= 0 && newY < n && grid[newX][newY] === "1") stack.push([newX,newY]);
-                    }
-                }
-                count++;
+    function dfs(x: number, y: number): void {
+        if (x < 0 || x >= numRows || y < 0 || y >= numCols || grid[x][y] === "0") {
+            return;
+        }
+        grid[x][y] = "0"; // Mark the visited cell
+        for (const [dx, dy] of directions) {
+            dfs(x + dx, y + dy);
+        }
+    }
+
+    for (let i = 0; i < numRows; i++) {
+        for (let j = 0; j < numCols; j++) {
+            if (grid[i][j] === "1") {
+                dfs(i, j);
+                islandCount++;
             }
         }
     }
 
-    return count;
-};
+    return islandCount;
+}
