0dc2e42fd882e3cfd939f6df171d4270cb59c421
Youssef Soua <youssefsoua94@gmail.com>
2024-05-04 01:43:43 +0100

    Time: 123 ms (84.21%) | Memory: 58.2 MB (12.28%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.ts
+++ 917-boats-to-save-people/boats-to-save-people.ts
@@ -1,21 +1,33 @@
 function numRescueBoats(people: number[], limit: number): number {
+    // Get the number of people
     const n = people.length;
+    
+    // Counter for the number of boats
     let count = 0;
+    
+    // Sorting the array of people in ascending order
     people.sort((a, b) => a - b);
 
+    // Pointers for the heaviest and lightest person
     let left = 0;
     let right = n - 1;
 
+    // Iterating through the array
     while (left <= right) {
+        // If the sum of weights of the lightest and heaviest person exceeds the limit
         if (people[left] + people[right] > limit) {
+            // Move the pointer of the heaviest person to the left (decreasing the weight)
             right--;
         } else {
+            // If the sum does not exceed the limit, both the lightest and heaviest person can go on the boat
             left++;
             right--;
         }
 
+        // Increment the boat count
         count++;
     }
 
+    // Return the total number of boats needed
     return count;
 };
