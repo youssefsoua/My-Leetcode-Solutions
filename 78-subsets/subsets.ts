37a6cee5905eb77c192abbe42661b0c5659c0bcd
Youssef Soua <youssefsoua94@gmail.com>
2024-05-21 19:49:22 +0100

    Time: 66 ms (60.16%) | Memory: 53.7 MB (46.20%) - LeetSync

--- 78-subsets/subsets.ts
+++ 78-subsets/subsets.ts
@@ -1,33 +1,13 @@
 function subsets(nums: number[]): number[][] {
-    const result: number[][] = [];
-
-    const subsetsCreator = (nums: number[], subset: number[]) => {
-        result.push([...subset]);
-        for (let i = 0; i < nums.length; i++) {
-            subset.push(nums[i]);
-            subsetsCreator(nums.slice(i + 1), subset);
-            subset.pop();
-        }
-    };
-
-    subsetsCreator(nums, []);
-
-    return result;
-}
-
-
-/**
-function subsets(nums: number[]): number[][] {
-    const n = nums.length;
     const result: number[][] = [[]];
+    const n = nums.length;
 
     for (let i = 0; i < n; i++) {
-        const c = result.length;
-        for (let j = 0; j < c; j++) {
-            const e = [...result[j], nums[i]];
-            result.push(e);
+        const m = result.length;
+        for (let j = 0; j < m; j++) {
+            result.push([...result[j], nums[i]]);
         }
     }
+
     return result;
-}
- */
+};
