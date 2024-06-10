437b0755c375dbb3b8f1423b31011c60dae6bab7
Youssef Soua <youssefsoua94@gmail.com>
2024-06-10 08:43:22 +0100

    Time: 58 ms (64.15%) | Memory: 51.4 MB (64.78%) - LeetSync

--- 1137-height-checker/height-checker.ts
+++ 1137-height-checker/height-checker.ts
@@ -1,10 +1,6 @@
 function heightChecker(heights: number[]): number {
-    let result: number = 0;
-    const sorted = heights.slice().sort((a, b) => a - b);
-
-    for (let i = 0; i < sorted.length; i++) {
-        if (sorted[i] !== heights[i]) result++;
-    }
-
-    return result;
-}
+    return heights
+        .slice()
+        .sort((a, b) => a - b)
+        .reduce((s, e, i) => s += (e !== heights[i] ? 1 : 0), 0);
+};
