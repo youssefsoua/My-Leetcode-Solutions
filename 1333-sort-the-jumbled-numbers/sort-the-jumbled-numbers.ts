660b0da5d2ec0104bbb478d132f75ab332a0641b
Youssef Soua <youssefsoua94@gmail.com>
2024-07-24 19:40:12 +0100

    Time: 456 ms (50.00%) | Memory: 78.6 MB (25.00%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.ts
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.ts
@@ -1,16 +1,4 @@
 function sortJumbled(mapping: number[], nums: number[]): number[] {
-    const mappedValues = nums.map(num => ({
-        original: num,
-        mapped: num.toString().split('').map(s => mapping[+s]).join('')
-    }));
-
-    mappedValues.sort((a, b) => +a.mapped - +b.mapped);
-
-    return mappedValues.map(mapped => mapped.original);
-}
-
-/*
-function sortJumbled(mapping: number[], nums: number[]): number[] {
     const mappedValues: number[][] = [];
 
     for (const num of nums) {
@@ -25,4 +13,3 @@ function sortJumbled(mapping: number[], nums: number[]): number[] {
 
     return mappedValues.map(e => e[0]);
 };
-*/
