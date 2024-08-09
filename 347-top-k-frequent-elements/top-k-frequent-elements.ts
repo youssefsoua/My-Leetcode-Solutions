e86079c2357f739c2a26b16c9d13da59a00ea983
Youssef Soua <youssefsoua94@gmail.com>
2024-08-09 21:47:19 +0100

    Time: 54 ms (98.55%) | Memory: 54.6 MB (66.67%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.ts
+++ 347-top-k-frequent-elements/top-k-frequent-elements.ts
@@ -2,11 +2,16 @@ function topKFrequent(nums: number[], k: number): number[] {
     const numbersCounter = new Map<number, number>();
 
     for (const num of nums) {
-        numbersCounter.set(num, (numbersCounter.get(num) || 0) + 1);
+        numbersCounter.set(num, (numbersCounter.get(num) ?? 0) + 1);
     }
-    
-    return Array.from(numbersCounter.entries())
-        .sort((a, b) => b[1] - a[1])
-        .slice(0, k)
-        .map((e) => e[0]);
-}
+
+    let result: number[] = [];
+
+    const counter = Array.from(numbersCounter.entries()).sort((a, b) => b[1] - a[1]);
+
+    for (let i = 0; i < k; i++) {
+        result.push(counter[i][0]);
+    }
+
+    return result;
+};
