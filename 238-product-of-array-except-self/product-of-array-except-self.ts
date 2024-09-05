6ef9c6ad336435cd452957330c6b668ab29e2d1e
Youssef Soua <youssefsoua94@gmail.com>
2024-09-05 22:35:16 +0100

    Time: 111 ms (42.99%) | Memory: 67.1 MB (33.20%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.ts
+++ 238-product-of-array-except-self/product-of-array-except-self.ts
@@ -1,18 +1,18 @@
 function productExceptSelf(nums: number[]): number[] {
-  const n: number = nums.length;
-  const result: number[] = new Array(n).fill(1);
+    const n: number = nums.length;
+    const result: number[] = new Array(n).fill(1);
 
-  let prefix: number = 1;
-  for (let i: number = 0; i < n; i++) {
-    result[i] *= prefix;
-    prefix *= nums[i];
-  }
+    let prefix: number = 1;
+    for (let i: number = 0; i < n; i++) {
+        result[i] *= prefix;
+        prefix *= nums[i];
+    }
 
-  let suffix: number = 1;
-  for (let i: number = n - 1; i >= 0; i--) {
-    result[i] *= suffix;
-    suffix *= nums[i];
-  }
+    let suffix: number = 1;
+    for (let i: number = n - 1; i >= 0; i--) {
+        result[i] *= suffix;
+        suffix *= nums[i];
+    }
 
-  return result;
+    return result;
 }
